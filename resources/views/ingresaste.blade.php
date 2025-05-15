2fa8fc7d7288cbb5213de866dbe6dbf9e8fe64d9
unknown <hipogea@hotmail.com>
2025-05-15 13:17:15 -0500

    Commit 2

--- resources/views/ingresaste.blade.php
+++ resources/views/ingresaste.blade.php
@@ -1,13 +1,58 @@
 <html>
+   <head>
+      @vite([ 'resources/css/app.css' , 'resources/js/app.js'])
+      <title> Bienvenida </title>
+   </head>
+   <script>
+   
+   </script>
+<body class="bg-light">
+     <nav class="navbar navbar-expand-lg  navbar-dark bg-primary">
+        <div class="container-fluid"> 
+          <a class="navbar-brand" href="#">CARTAGO</a>
+           
+          <span class="navbar-text text-white me-3">
+            <i  class="fa fa-user"><i>   {{auth()->user()->name}}
+          </span>
 
-<body>
-   <h1> HOLA   {{auth()->user()->name}}  BIENVENIDO A LA PLATAFORMA  </h1>
 
-   <a href="/miperfil">  IR A MI PERFIL   </a>
-
+        </div> 
+     </nav>
 
 
-   <a href="/salir">  Salir de sesion   </a>
+  <div class="container-fluid">
+      <div class="row">
+         <div class="col-md-3 col-lg-2 bg-white border-end vh-100 p-3">
+              <h6> Mi menu </h6>
+              <ul class="nav flex-column">
+               <li class="nav-item"> <a href="/miperfil">Mi perfil</a> </li>
+               <li class="nav-item"> <a href="#"></a> Opcion 1</li>
+               <li class="nav-item"> <a href="#"></a> Opcion 2</li>
+               <li class="nav-item"> <a href="#"></a> Opcion 3 </li>
+               <li class="nav-item"> <a href="#"></a> Opcion 4</li>
+
+           </ul>
+         </div> 
+      
+ 
+
+
+
+
+     <div class="col-md-9 col-lg-10  p-4">
+        <h1> HOLA   {{auth()->user()->name}}  BIENVENIDO A LA PLATAFORMA  </h1>
+      <a href="/miperfil">  IR A MI PERFIL   </a>
+      <a href="/salir">  Salir de sesion   </a>
+     </div>
+   
+   </div>
+</div>
+      <footer class="bg-primary text-white text-center py-3 mt-auto">
+         <div class="containter">
+           <small>&copy;{{date('Y-m')}} CARTAGO -Derechos reservados
+         </div>
+       </footer>
+
 
 </body>
 
--- resources/views/show-login.blade.php
+++ resources/views/show-login.blade.php
@@ -1,26 +1,38 @@
-<html>
 
-<body>
-  <h1> AUTENTICACION </h1>
+<html>
+<head>
+   @vite([ 'resources/css/app.css' , 'resources/js/app.js'])
+   <title> Auntenticación </title>
+</head>
+<script>
+
+</script>
+<body class="bg-light d-flex justify-content-center align-items-center vh-100">
+   
+   <div class="card shadow p-4 " style="width:100%; max-width:400px;">
+      <span class="mi-letra-verde"> AUTENTICACION </span>
+
     <form  method="POST"  action="/bienvenida">
         @csrf <!-- Aseguras que la clave secreta viaje en el método POST -->
         <div>
            <label>Correo</label>
-           <input type="text" name="email" required  value="{{old('email')}}"><br>
+           <input  class="form-control" type="text" name="email" required  value="{{old('email')}}"><br>
            @error('email')
               <span  style="color:red">{{$message}}</span>
            @enderror
         </div>
         <div>
             <label>Contraseña</label>
-            <input type="password" name="password" required >
+            <input  class="form-control" type="password" name="password" required >
          </div>
          @error('password')
          <span  style="color:red">{{$message}}</span><br>
          @enderror
-        <button type="submit">Autenticar</button> <!--boton  -- >
+         <div class="d-grid mt-4" >
+        <button  class="btn btn-info" type="submit">Autenticar</button> <!--boton  -- >
+        </div>
     </form>
-
+   </div>
 
 
 
